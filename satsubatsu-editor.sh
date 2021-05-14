d59e3fc90a8398c1d4804f5e4a25e3f1903a2100
Sheepla <plusplus.sheep@gmail.com>
2021-05-14 21:30:23 +0900

    [update]

---
+++ satsubatsu-editor.sh
@@ -0,0 +1,3 @@
+echo-sd 殺伐としたTLに 突然Visual\ Studio\ Codeが！ 
+#
+paste -d "" <(echo-sd "　 "{,,,}) <(figlet -f slant "Emacs"; echo ※Atom（さぶらいむてきすと）です) | lolcat -f -p 0.2 | textimg -F50 -bwhite | composite -gravity west -geometry +50-25 \( -resize 150x150 /usr/share/icons/hicolor/48x48/apps/gvim.png \) - /images/a #シェル芸
---
+++ yurucamp-logo.sh
@@ -0,0 +1,4 @@
+textimg $' ● ● ●　' -g240,140,60,255 -bwhite -F100 | composite -gravity center -geometry +45 <(textimg '● ● ●' -F100 -g127,204,217,255 -b0,0,0,0)  - - | #
+#
+composite -gravity south <(<<< ＹＵＲＵＣＡＭＰ sed 's_._　&_g' | textimg -g100,100,100,255 -b0,0,0,0 -F15) - /images/x.png
+#シェル芸
