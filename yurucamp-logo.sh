a94ce6a63df08a16ba06d1b45351d199aefd128a
Sheepla <plusplus.sheep@gmail.com>
2021-05-14 22:36:15 +0900

    [update]

--- yurucamp-logo.sh
+++ yurucamp-logo.sh
@@ -1,4 +1,6 @@
 textimg $' ● ● ●　' -g240,140,60,255 -bwhite -F100 | composite -gravity center -geometry +45 <(textimg '● ● ●' -F100 -g127,204,217,255 -b0,0,0,0)  - - | #
 #
+composite -gravity center -geometry +10 <(textimg 'ゆ る キ ャ ン ▲' -b0,0,0,0 -F50) - - | #
+#
 composite -gravity south <(<<< ＹＵＲＵＣＡＭＰ sed 's_._　&_g' | textimg -g100,100,100,255 -b0,0,0,0 -F15) - /images/x.png
 #シェル芸
